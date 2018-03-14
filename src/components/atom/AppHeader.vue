bcfcabee0084fbf13f2d66d7667fad1680f0f6a9
alextanhongpin <alextan@seekasia.com>
2018-03-15 00:23:19 +0800

    chore: initial commit

---
+++ src/components/atom/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="brand">
+      {{name}}
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  props: {
+    name: {
+      type: String,
+      required: true,
+      default: 'AppName'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import "../../styles/theme.scss";
+
+header {
+  height: 60px;
+  line-height: 60px;
+}
+.brand {
+  color: $coral-red;
+  font-weight: bold;
+  border: 2px solid $coral-red;
+  height: 30px;
+  line-height: 30px;
+  display: inline-block;
+  padding: 0 20px;
+}
+</style>
---
+++ src/components/page/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <app-header name="Bank Voice"/>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+import AppHeader from '../atom/AppHeader.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    AppHeader
+  },
+  data () {
+    return {
+      msg: 'The open platform that allows you to voice out your thoughts'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
